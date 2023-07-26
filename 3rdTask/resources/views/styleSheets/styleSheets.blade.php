631001f5a46a3b85a2ab06736bdd6c62ec33d13d
Aligh <alighaforian@yahoo.com>
2023-07-26 11:11:22 +0330

    made style Sheet blade file to store CSS files
    added CSS plugins and Style Sheet blade file

---
+++ 3rdTask/resources/views/styleSheets/styleSheets.blade.php
@@ -0,0 +1,25 @@
+<!-- Font Awesome -->
+<link rel="stylesheet" href="{{asset('plugins/font-awesome/css/font-awesome.min.css')}}">
+<!-- Ionicons -->
+<link rel="stylesheet" href="{{asset('plugins/ionicons/css/ionicons.min.css')}}">
+<!-- Theme style -->
+<link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+<!-- iCheck -->
+<link rel="stylesheet" href="{{asset('plugins/iCheck/flat/blue.css')}}">
+<!-- Morris chart -->
+<link rel="stylesheet" href="{{asset('plugins/morris/morris.css')}}">
+<!-- jvectormap  -->
+<link rel="stylesheet" href="{{asset('plugins/jvectormap/jquery-jvectormap-1.2.2.css')}}">
+<!-- Date Picker -->
+<link rel="stylesheet" href="{{asset('plugins/datepicker/datepicker3.css')}}">
+<!-- Daterange picker -->
+<link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker-bs3.css')}}">
+<!-- bootstrap wysihtml5 - text editor -->
+<link rel="stylesheet" href="{{asset('plugins/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css')}}">
+<!-- Google Font: Source Sans Pro -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
+<!-- bootstrap rtl -->
+<link rel="stylesheet" href="{{asset('dist/css/bootstrap-rtl.min.css')}}">
+<!-- template rtl version -->
+<link rel="stylesheet" href="{{asset('dist/css/custom-style.css')}}">
+
