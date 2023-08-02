b7ae5fee20af2d1f0055b17dfe7b28c53ecc7471
Aligh <alighaforian@yahoo.com>
2023-08-02 13:39:43 +0330

    added more input to users migration and input

--- database/migrations/2023_08_01_170903_create_customers_table.php
+++ database/migrations/2023_08_01_170903_create_customers_table.php
@@ -13,10 +13,21 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('email',50)->unique();
             $table->string('first_name',30);
             $table->string('last_name',30);
-            $table->integer('phone_number')->unique()->nullable(FALSE);
-            $table->string('email',50)->unique()->nullable(FALSE);
+            $table->string('user_name',30)->unique();
+            $table->integer('phone_number');
+            $table->integer('age');
+            $table->string('gender');
+            $table->string('education');
+            $table->string('occupation');
+            $table->text('interests')->nullable();
+            $table->text('hobbies')->nullable();
+            $table->text('bio')->nullable();
+            $table->integer('postal_code');
+            $table->string('address');
+            $table->string('password');
             $table->bigInteger('total_bought')->unsigned()->default(0);
             $table->timestamps();
         });
--- resources/views/users/addUser.blade.php
+++ resources/views/users/addUser.blade.php
@@ -38,34 +38,88 @@
                     <div class="card-body">
                         <div class="form-group">
                             <label for="email">ایمیل</label>
-                            <input type="email" class="form-control" id="email" name="email"
-                                   placeholder="ایمیل را وارد کنید">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="ایمیل را وارد کنید">
                         </div>
                         <div class="form-group">
                             <label for="first_name">نام</label>
-                            <input type="text" class="form-control" id="first_name" name="first_name"
-                                   placeholder="نام">
+                            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="نام">
                         </div>
                         <div class="form-group">
                             <label for="last_name">نام خانوادگی</label>
-                            <input type="text" class="form-control" id="last_name" name="last_name"
-                                   placeholder="نام خانوادگی">
+                            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="نام خانوادگی">
                         </div>
                         <div class="form-group">
                             <label for="user_name">نام کاربری</label>
-                            <input type="text" class="form-control" id="user_name" name="user_name"
-                                   placeholder="نام کاربری">
+                            <input type="text" class="form-control" id="user_name" name="user_name" placeholder="نام کاربری">
                         </div>
                         <div class="form-group">
                             <label for="phone_number">شماره همراه</label>
-                            <input type="text" class="form-control" id="phone_number" name="phone_number"
-                                   placeholder="شماره همراه">
+                            <input type="number" class="form-control" id="phone_number" name="phone_number" placeholder="9120000000">
+                        </div>
+
+                        <!-- Add the remaining input fields -->
+                        <div class="form-group">
+                            <label for="age">سن</label>
+                            <input type="number" class="form-control" id="age" name="age" placeholder="سن را وارد کنید">
                         </div>
 
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1"
-                                   placeholder="پسورد را وارد کنید">
+                            <label for="gender">جنسیت</label>
+                            <select class="form-control" id="gender" name="gender">
+                                <option value="male">مرد</option>
+                                <option value="female">زن</option>
+                                <option value="other">سایر</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="education">تحصیلات</label>
+                            <select class="form-control" id="education" name="education">
+                                <option value="high_school">دیپلم</option>
+                                <option value="bachelor">کارشناسی</option>
+                                <option value="master">کارشناسی ارشد</option>
+                                <option value="doctorate">دکتری</option>
+                            </select>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="occupation">شغل</label>
+                            <input type="text" class="form-control" id="occupation" name="occupation" placeholder="شغل را وارد کنید">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="interests">علایق</label>
+                            <textarea class="form-control" id="interests" name="interests" placeholder="علایق خود را وارد کنید"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="hobbies">سرگرمی‌ها</label>
+                            <textarea class="form-control" id="hobbies" name="hobbies" placeholder="سرگرمی‌های خود را وارد کنید"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="bio">درباره‌ی خود</label>
+                            <textarea class="form-control" id="bio" name="bio" placeholder="درباره‌ی خودتان بنویسید"></textarea>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="postal_code">کد پستی</label>
+                            <input type="number" class="form-control" id="postal_code" name="postal_code" placeholder="کد پستی را وارد کنید">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="address">آدرس</label>
+                            <input type="text" class="form-control" id="address" name="address" placeholder="آدرس را وارد کنید">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password">پسورد</label>
+                            <input type="password" class="form-control" id="password" name="password" placeholder="پسورد را وارد کنید">
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirm">تکرار پسورد</label>
+                            <input type="password" class="form-control" id="password_confirm" name="password_confirm" placeholder="پسورد را دوباره وارد کنید">
                         </div>
                     </div>
                     <!-- /.card-body -->
