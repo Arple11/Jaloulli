93669d6752ba7b352930fe81773def222ae24119
Aligh <alighaforian@yahoo.com>
2023-08-06 21:18:52 +0330

    added Product.php for Product model, also edited ProductController.php, so it will use models instead of query builder.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,101 +10,38 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
-use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
 
     public function store(Request $request): RedirectResponse
     {
-
-        $imageUrls = [];// an array of images urls so that wi can store it as we want
-        if ($images = $request->file('productImages')) {
-            foreach ($images as $image) {
-                $name = $image->getClientOriginalName();
-                $image->move("productImages/{$request->post('productName')}", $name);
-                $imageUrls[] = "productImages/{$request->post('productName')}/" . "$name";
-            }
-        }
-        $product = Product::create($request->all());
-        #inserting [ product id, image url ] to product_images table
-        foreach ($imageUrls as $imageUrl) {
-            product_image::create([
-                'product_id' => $product->id,
-                'image_url' => $imageUrl,
-            ]);
-        }
+        Product::saveWithImage($request);
         return redirect()->route('Products_data');
     }
 
     public function all_products(): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        #storing useful data in $datas (making sure the data is enable)
-        $datas = (DB::table('products')
-            ->where('enable', '=', 1)
-            ->select([
-                'id',
-                'product_name',
-                'explanation',
-                'price',
-                'amount_available',
-            ])->get());
-        #serching in product_images table for images related to each data set
-        $imagesArr = [];
-        foreach ($datas as $data) {
-            $images = DB::table('product_images')
-                ->where('product_id', '=', $data->id)
-                ->select('image_url')
-                ->get();
-            $imagesArr["$data->id"] = $images;
-        }
-        #making an array for passing to view
-        $allData = [
-            'products' => $datas,
-            'productsImages' => $imagesArr
-        ];
         #opening productsData view with the passing data
-        return view('products.productsData')->with(['allData' => $allData]);
+        return view('products.productsData')->with(['allData' => Product::getAllProductsWithImage()]);
     }
 
     public function delete_product($id): RedirectResponse
     {
-        DB::table('products')
-            ->where('id', '=', $id)
-            ->update([
-                'enable' => 0,
-                'updated_at' => now(),
-            ]);
+        Product::destroy($id);
         return redirect()->route('Products_data');
     }
 
     public function edit_product_menu($id): View|Application|Factory|\Illuminate\Contracts\Foundation\Application
     {
-        $data = DB::table('products')
-            ->where('id', '=', $id)
-            ->select([
-                'id',
-                'product_name',
-                'explanation',
-                'price',
-                'amount_available',
-                'amount_sold',
-            ])->first();
-        return view('products.editProductMenue', ['product' => $data]);
+        return view('products.editProductMenue', ['product' => Product::editSelect($id)]);
     }
 
     public function store_edited_product(Request $request, $id): RedirectResponse
     {
-        DB::table('products')
-            ->where('id', '=', $id)
-            ->update([
-                'product_name' => $request->post('product_name'),
-                'explanation' => $request->post('explanation'),
-                'price' => $request->post('price'),
-                'amount_available' => $request->post('amount_available'),
-                'amount_sold' => $request->post('amount_sold'),
-                'updated_at' => now(),
-            ]);
+        Product::saveEditedProduct($request,$id);
         return redirect()->route('Products_data');
     }
+
+
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,22 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Http\Request ;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Builder;
+use phpDocumentor\Reflection\Types\Integer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\product_image;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
 /**
  * App\Models\Product
  *
- * @property int $id
- * @property string $product_name
- * @property string $explanation
- * @property int $price
- * @property int $amount_available
- * @property int $amount_sold
- * @property string|null $tags
- * @property int $enable
+ * @property int                             $id
+ * @property string                          $product_name
+ * @property string                          $explanation
+ * @property int                             $price
+ * @property int                             $amount_available
+ * @property int                             $amount_sold
+ * @property string|null                     $tags
+ * @property int                             $enable
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Database\Factories\ProductFactory factory($count = null, $state = [])
@@ -38,7 +44,7 @@ use App\Models\product_image;
  */
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'product_name',
@@ -50,9 +56,8 @@ class Product extends Model
         'enable',
     ];
 
-/*    public static function create(): Builder|Product
+    public static function saveWithImage(Request $request): Builder|Product
     {
-        $request = request();
         $imageUrls = [];// an array of images urls so that we can store it as we want
         if ($images = $request->file('productImages')) {
             foreach ($images as $image) {
@@ -61,8 +66,7 @@ class Product extends Model
                 $imageUrls[] = "productImages/{$request->product_name}/" . "$name";
             }
         }
-        $product = parent::create($request->all());
-
+        $product = Product::create($request->all());
         #inserting [ product id, image url ] to product_images table
         foreach ($imageUrls as $imageUrl) {
             product_image::create([
@@ -71,5 +75,64 @@ class Product extends Model
             ]);
         }
         return Product::whereId($product->id);
-    }*/
+    }
+
+    public static function getAllProductsWithImage()
+    {
+        $oldTrashs = Product::onlyTrashed()
+            ->where('deleted_at', '<=', Carbon::now()->subDays(90))
+            ->get();
+        foreach ($oldTrashs as $oldTrash)
+            $oldTrash->forceDelete();
+        #storing useful data in $datas (making sure the data is enable)
+        $datas = Product::select([
+            'id',
+            'product_name',
+            'explanation',
+            'price',
+            'amount_available',
+        ])->get();
+        #serching in product_images table for images related to each data set
+        $imagesArr = [];
+        foreach ($datas as $data) {
+            $images = product_image::whereProductId($data->id)
+                ->select('image_url')
+                ->get();
+            $imagesArr["$data->id"] = $images;
+        }
+        #making an array for passing to view
+        $allData = [
+            'products' => $datas,
+            'productsImages' => $imagesArr
+        ];
+        return $allData;
+    }
+
+    public static function editSelect(int $id)
+    {
+        return Product::find($id)
+            ->select([
+                'id',
+                'product_name',
+                'explanation',
+                'price',
+                'amount_available',
+                'amount_sold',
+            ]);
+    }
+
+    public static function saveEditedProduct(Request $request, int $id)
+    {
+        $product = Product::find($id);
+        $product->fill([
+            'product_name' => $request->product_name,
+            'explanation' => $request->explanation,
+            'price' => $request->price,
+            'amount_available' => $request->amount_available,
+            'amount_sold' => $request->amount_sold,
+        ]);
+        $product->save();
+        return $product;
+    }
 }
+
