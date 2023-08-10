1423deb7eb4c69bd3a06a9ef8c6b001dad30f9e8
Aligh <alighaforian@yahoo.com>
2023-08-11 03:23:58 +0330

    made some adjustments for bt5 JS

--- resources/views/orders/addOrder.blade.php
+++ resources/views/orders/addOrder.blade.php
@@ -9,7 +9,7 @@
     @include('styleSheets.styleSheets')
     <link rel="stylesheet" href="{{asset('persenalCss/app.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="{{asset('js/bt5.js')}}"></script>
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
